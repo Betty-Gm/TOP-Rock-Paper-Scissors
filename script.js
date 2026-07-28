4c96203e9ec39fb433e31fe8ccc3c7fbac9d4ebd
bethlehem <bethlehemgebremichael11@gmail.com>
2026-07-28 15:41:31 +0300

    Add getComputerChoice function

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log(1)
+function getComputerChoice(){
+    let cprChoice= Math.floor(Math.random()*3);
+    
+    if (cprChoice==0){
+        cprChoice= "rock";
+    }
+    else if(cprChoice==1){
+        cprChoice="paper";
+    }
+    else {
+        cprChoice="scissors";
+    }
+  
+    return cprChoice;
+
+
+
+}
