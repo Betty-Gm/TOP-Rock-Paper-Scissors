0af111748ae97df31166439dd9b9827e11a627a7
bethlehem <bethlehemgebremichael11@gmail.com>
2026-07-28 17:27:08 +0300

    Add playGame function to run full 5-round game and declare the winner

--- script.js
+++ script.js
@@ -33,33 +33,43 @@ function getHumanChoice(){
 }
 
 // function to play in rounds
+
+
+
 function playRound(humanChoice, computerChoice){
+    
 
     console.log(`your choice: ${humanChoice}
 computer choice: ${computerChoice}\n`);
 
     if(humanChoice=="rock" && computerChoice=="scissors"){
         console.log("you won, Rock beats Scissors");
+        humanScore++;
 
     }
     else if(humanChoice=="scissors" && computerChoice=="rock"){
         console.log("you lose, Rock beats Scissors");
+        computerScore++;
 
     }
     else if(humanChoice=="scissors" && computerChoice=="paper"){
         console.log("you won, Scissors beats Paper");
+        humanScore++;
 
     }
     else if(humanChoice=="paper" && computerChoice=="scissors"){
         console.log("you lose, Scissors beats Paper");
+        computerScore++;
 
     }
     else if(humanChoice=="paper" && computerChoice=="rock"){
         console.log("you won, Paper beats Rock");
+        humanScore++;
 
     }
     else if (humanChoice=="rock" && computerChoice== "paper"){
         console.log("you lose, Paper beats Rock");
+        computerScore++;
 
     }
     else{ 
@@ -69,8 +79,29 @@ computer choice: ${computerChoice}\n`);
 
 }
 
-const humanSelection= getHumanChoice();
-const computerSelection= getComputerChoice();
 
-playRound(humanSelection,computerSelection);
 
+
+// a function that tracks scores and decide the winner
+function playGame(){
+    for (let round=1;round<=5; round++){
+        console.log(`This is round ${round}`);
+        const humanSelection= getHumanChoice();
+        const computerSelection= getComputerChoice();
+        playRound(humanSelection,computerSelection);
+        
+
+    }
+    if (humanScore>computerScore){
+        console.log(`Congratulations! you are the winner`);
+
+    }
+    else if(computerScore>humanScore){
+        console.log("Oops, you lose.");
+    }
+    else{
+        console.log("Oops, its a tie.")
+    }
+}
+
+playGame();
